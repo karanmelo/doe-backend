53ab427e795a19023fa533f21d628de080ba30ae
Karan Melo <karan_melo@hotmail.com>
2022-03-02 20:47:26 -0300

    feat: multer registry

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { MulterModule } from '@nestjs/platform-express';
 
 import { serviceConfig } from 'src/configs/service.config';
 import { InstitutionsConroller } from 'src/controllers';
 import { Institution, InstitutionSchema } from 'src/entities';
+import { MulterConfigService } from 'src/plugins';
 import { InstitutionsProvider } from 'src/providers';
 import { InstitutionsService } from 'src/services';
 
@@ -13,6 +15,9 @@ import { InstitutionsService } from 'src/services';
       { name: Institution.name, schema: InstitutionSchema },
     ]),
     MongooseModule.forRoot(serviceConfig.mongoUrl),
+    MulterModule.registerAsync({
+      useClass: MulterConfigService,
+    }),
   ],
   providers: [InstitutionsProvider, InstitutionsService],
   controllers: [InstitutionsConroller],
