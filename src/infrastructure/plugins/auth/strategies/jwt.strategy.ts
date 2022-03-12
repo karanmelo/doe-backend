40279ca14461b7c4d41c2f51bbe7a67264f83c56
Karan Melo <karan_melo@hotmail.com>
2022-03-12 20:05:35 -0300

    fix: jwt strategy

--- src/infrastructure/plugins/auth/strategies/jwt.strategy.ts
+++ src/infrastructure/plugins/auth/strategies/jwt.strategy.ts
@@ -1,13 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 
 import { ExtractJwt, Strategy } from 'passport-jwt';
 
+import { IAuthenticator } from 'src/core/commons/interfaces';
 import { serviceConfig } from 'src/infrastructure/configs/service.config';
+
+import { AuthService } from '../auth.service';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
-  constructor() {
+  constructor(private authService: AuthService) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
@@ -15,7 +18,11 @@ export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
     });
   }
 
-  async validate(payload: any) {
-    return { clientId: payload.clientId, clienteToken: payload.clienteToken };
+  async validate(payload: IAuthenticator) {
+    const validated = await this.authService.validate(payload);
+    if (!validated) {
+      throw new UnauthorizedException();
+    }
+    return payload;
   }
 }
