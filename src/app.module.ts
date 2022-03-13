73a384da5953ec0d427e7968b8d9dff7a2000830
Karan Melo <karan_melo@hotmail.com>
2022-03-13 15:11:47 -0300

    fix: update multer service to local and s3 storages

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,8 +5,8 @@ import { MulterModule } from '@nestjs/platform-express';
 import { ChallengeController, InstitutionsConroller } from 'src/controllers';
 import { InstitutionRepositoryModule } from 'src/infrastructure/adapters/database/repositories/institution/institution.module';
 import { serviceConfig } from 'src/infrastructure/configs/service.config';
-import { MulterConfigService } from 'src/infrastructure/plugins';
 import { AuthPluginModule } from 'src/infrastructure/plugins/auth/auth.module';
+import { MulterService } from 'src/infrastructure/plugins/multer';
 import {
   CreateInstitutionService,
   ChallengeAuthenticatorService,
@@ -18,7 +18,7 @@ import { InstitutionsService } from 'src/service/queries';
     AuthPluginModule,
     MongooseModule.forRoot(serviceConfig.mongoUrl),
     MulterModule.registerAsync({
-      useClass: MulterConfigService,
+      useClass: MulterService,
     }),
     InstitutionRepositoryModule,
   ],
